eac39a4e1981af6b6c2c231564077be6d0ed0dd2
crowwalk <crowwalk@icloud.com>
2024-09-26 10:40:23 -0400

    New Update To My project

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initalCards = [
+const initialCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
@@ -25,14 +25,36 @@ const initalCards = [
   },
 ];
 
-console.log(initalCards);
-
 const profileButtonEdit = document.querySelector(".profile__button-edit");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-modal");
+const editFormElement = editModal.querySelector(".modal__form");
 const editModalCloseBtn = editModal.querySelector(".modal__close-button");
+const editModalNameInput = editModal.querySelector("#profile-name-input");
+const editModalDescriptionInput = editModal.querySelector(
+  "#profile-description-input"
+);
+
+const cardTemplate = document.querySelector("#card-template");
+
+function getCardElement(data) {
+  console.log(data);
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__caption");
+
+  cardNameEl.textContent = data.name;
+
+  return cardElement;
+}
 
 function openModal() {
+  editModalDescriptionInput.value = profileDescription.textContent;
+  editModalNameInput.value = profileName.textContent;
   editModal.classList.add("modal_opened");
 }
 
@@ -40,6 +62,17 @@ function closeModal() {
   editModal.classList.remove("modal_opened");
 }
 
-profileButtonEdit.addEventListener("click", openModal);
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+  closeModal();
+}
 
+profileButtonEdit.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  getCardElement(initialCards[i]);
+}
