1b7b513fdfd3ebfb37964ac22eebef8a6ea6d3d8
crowwalk <crowwalk@icloud.com>
2024-10-19 17:35:30 -0400

    New Update for My project

---
+++ scripts/validation.js
@@ -0,0 +1,10 @@
+const settings = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__submit-button",
+  inactiveButtonClass: ".modal__submit-button_disabled",
+  inputErrorClass: "",
+  errorClass: ".modal__error",
+};
+
+//const enableValidation ("settings");
